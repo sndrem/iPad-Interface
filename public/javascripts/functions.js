7408d55dabc9ae21ca09fd85d68cb3f63eeb1650
Sindre <sndrem@live.no>
2017-12-03 22:46:38 +0100

    Removed some buttons, added a global sound button and refactored part of the code

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,8 +1,16 @@
-const buttons = Array.from(document.querySelectorAll(".btn"));
+const buttons = Array.from(document.querySelectorAll(".timeTableBtn"));
+const soundBtn = document.getElementById("soundBtn");
 const clockStatus = document.querySelector(".clock-status");
 const statusMessageElement = document.querySelector(".status");
 
 let dataFetchInterval = null;
+
+let silentTTS = false;
+if(window.localStorage.getItem("sound")) {
+    silentTTS = JSON.parse(window.localStorage.getItem("sound"));
+}
+
+updateSoundButton(soundBtn);
 
 const config = {
     DATA_FETCH_RATE: 20000
@@ -11,11 +19,15 @@ const config = {
 buttons.forEach(function(btn) {
     btn.addEventListener("click", startGettingData);
 });
+
 buttons.forEach(function(btn) {
     btn.addEventListener("touchstart", startGettingData)
 });
 
+soundBtn.addEventListener('click', toggleSound);
+
 moment.locale("nb");
+
 setInterval(function() {
     clockStatus.innerHTML = moment().format('LLLL');
 }, 1000);
@@ -32,28 +44,60 @@ function startGettingData() {
 function buttonClicked(btn) {
     const from = btn.dataset.from;
     const to = btn.dataset.to;
-    const silent = btn.dataset.silent;
     
     const params = {
         from: from,
         to: to,
-        silent: silent
+        silent: silentTTS
     }
+
 
     statusMessageElement.innerHTML = `Henter data for Bybanen<br>fra: <span class="destination">${from}</span><br>til: <span class="destination">${to}</span>`;
     getSkyssTimeTable(params);
     return setInterval(function() {
         getSkyssTimeTable(params);
     }, config.DATA_FETCH_RATE);
+}
 
-    
+function saveToLocalStorage(key, value) {
+    if(!key) {
+        throw new Error("Key cannot be empty");
+    } else if(!value) {
+        throw new Error("Value cannot be empty");
+    }
+
+    window.localStorage.setItem(key, value);
+}
+
+function toggleSound() {
+    saveToLocalStorage("sound", silentTTS.toString());
+    updateSoundButton(this);
+}
+
+function updateSoundButton(btn) {
+    const statusIcon = document.createElement("i");
+    statusIcon.classList.add("fa");
+    if(silentTTS) {
+        statusIcon.classList.add("fa-volume-up", "fa-green");
+        statusIcon.classList.remove("fa-volume-off", "fa-red");
+        btn.innerHTML = "Lyd på";
+        btn.appendChild(statusIcon);
+        silentTTS = false;
+    } else {
+        statusIcon.classList.add("fa-volume-off", "fa-red");
+        statusIcon.classList.remove("fa-volume-up", "fa-green");
+        btn.innerHTML = "Lyd av";
+        btn.appendChild(statusIcon);
+        silentTTS = true;
+    }
 }
 
 function getSkyssTimeTable(params) {
-    if(!params || !params.from || !params.to || !params.silent) {
+    if(!params || !params.from || !params.to) {
         throw new skyssTimeTableException("The params object needs a field for from, to, and silent");
         return;
     }
+
     var skyssRequest = new XMLHttpRequest();
     skyssRequest.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
