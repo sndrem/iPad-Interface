40e7882becd1de0d0b5b5ba2bdc6a00f229419c4
Sindre <sndrem@live.no>
2017-12-05 16:52:03 +0100

    Trying to fix the chart to look pretty

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -169,6 +169,10 @@ function drawChart(rainData) {
           chart: {
             title: 'Blir det regn neste 1,5 time?',
             subtitle: 'Data fra Yr.no',
+          },
+          vAxis: {
+            ticks: [0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 20.0],
+            title: "mm nedbør"
           }
         };
 
